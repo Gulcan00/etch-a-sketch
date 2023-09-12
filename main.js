38f870837e864cf491ca2075df1dee505e856e04
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2023-09-12 11:47:05 +0300

    Change background color on mouse enter

--- main.js
+++ main.js
@@ -5,9 +5,13 @@ function createDivs(total) {
     for (let i = 0; i < total; i++) {
         const newDiv = document.createElement("div");
         newDiv.classList.add("square");
+        newDiv.addEventListener("mouseenter", changeBackground);
         container.appendChild(newDiv);
     }
 }
 
+function changeBackground(e) {
+    e.target.style.backgroundColor = "black";
+}
 
 createDivs(16*16);
